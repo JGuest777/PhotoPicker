d45759b90f17f09491686e3ca6df24848462b06c
james <jguest777@gmail.com>
2020-07-23 15:10:41 -0400

    required env in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+require('dotenv').config();
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
